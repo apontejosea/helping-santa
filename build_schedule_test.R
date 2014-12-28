a4334a276d2b84822dd2e1a04fe515b54b1b744f
Yara Luis <Yara@Yaras-MacBook-Pro.local>
2014-12-28 13:07:14 -0700

    Yara's first commit

--- build_schedule_test.R
+++ build_schedule_test.R
@@ -4,6 +4,7 @@
 ########################################################
 library(GA)
 library(elves)
+library(GA)
 options(width=600)
 Sys.setenv(TZ="UTC")
 
@@ -18,27 +19,24 @@ toys_file_name     <- 'data/toys_rev2.csv'
 #=======================================================
 # Functions
 #=======================================================
-
 CreateSubmission <- function(schedule, file_name) {
-  submission_data <- subset(schedule,T,c(ToyId, ElfId, start, end))
-  submission_data$Duration <- ceiling(as.numeric(submission_data$end - submission_data$start)/60)
-  submission_data <- subset(submission_data,T,-end)
-  setnames(submission_data, c('ToyId','ElfId','StartTime','Duration'))
-  submission_data[,StartTime:=strftime(StartTime, '%Y %m %d %H %M')]
-  write.csv(submission_data, file=file_name, row.names=F)
+    #duration is calculated on .cpp
+    submission_data <- subset(schedule,T,c(ToyId, ElfId, start, end, duration))
+    submission_data <- subset(submission_data,T,-end)
+    setnames(submission_data, c('ToyId','ElfId','StartTime','Duration'))
+    submission_data[,StartTime:=strftime(StartTime, '%Y %m %d %H %M')]
+    write.csv(submission_data, file=file_name, row.names=F)
 }
-
-
 #=======================================================
 # Steps
 #=======================================================
 print(system.time(toys       <- read_toys(toys_file_name, nrows=(n_toys+1))))
 
-# S              <- toys$ToyId
-# result         <- CalculateObjective(S, toys, 4)
-# curr_objective <- result$objective
-# schedule       <- result$schedule
-# CreateSubmission(schedule, 'our_sub.csv')
+ S              <- toys$ToyId
+ result         <- CalculateObjective(S, toys, 4)
+ curr_objective <- result$objective
+ schedule       <- result$schedule
+ CreateSubmission(schedule, 'our_sub.csv')
 
 # GA <- ga(type = 'permutation', fitness = CalculateFitness, toys=toys, n_elves=n_elves, min=1, max=max(toys$ToyId), popSize=5, maxiter = 10, run=10, pmutation = 0.4)
 
@@ -47,25 +45,27 @@ print(system.time(toys       <- read_toys(toys_file_name, nrows=(n_toys+1))))
 # opt_toys<-opt_toys[order(opt_toys$ElfId,as.numeric(format(opt_toys$Arrival,"%m")),opt_toys$Duration)]
 # print(system.time(opt_schedule <- build_schedule_c(opt_toys, .parallel=T)))
 
-print(system.time(org_toys     <- distribute_toys(n_elves, toys)))
-org_toys<-org_toys[order(org_toys$ElfId,as.numeric(format(org_toys$Arrival,"%m")),org_toys$Duration)]
-print(system.time(org_schedule <- build_schedule_c(org_toys, .parallel=T)))
+#print(system.time(opt_toys     <- distribute_toys(n_elves, toys[as.numeric(GA@solution)])))
+#print(system.time(org_toys     <- distribute_toys(n_elves, toys)))
+#print(system.time(opt_schedule <- build_schedule_c(opt_toys, .parallel=T)))
+#print(system.time(org_schedule <- build_schedule_c(org_toys, .parallel=T)))
 
-CreateSubmission(org_schedule, 'my_submission.csv')
 
-# Uncomment to generate graphs
-# last_subset <- 100
-# PngIt(plot_duration(tail(schedule_c, last_subset)), 'toy_duration_c.png')
-# PngIt(plot_toy_schedule(tail(schedule_c, last_subset)), 'overall_toy_schedule_c.png')
-# PngIt(plot_elf_schedule(tail(schedule_c, last_subset)), 'overall_elf_schedule_c.png')
-  # PngIt(plot_elf_p_trend(tail(schedule_c, last_subset)), 'elf_p_trend_c.png')
+CreateSubmission(opt_schedule, 'our_sub.csv')
 
-# PngIt(plot_duration(opt_schedule), 'toy_duration_opt.png')
-# PngIt(plot_toy_schedule(opt_schedule), 'overall_toy_schedule_opt.png')
-# PngIt(plot_elf_schedule(opt_schedule), 'overall_elf_schedule_opt.png')
-# PngIt(plot_elf_p_trend(opt_schedule), 'elf_p_trend_opt.png')
-# 
-# PngIt(plot_duration(org_schedule), 'toy_duration_org.png')
-# PngIt(plot_toy_schedule(org_schedule), 'overall_toy_schedule_org.png')
-# PngIt(plot_elf_schedule(org_schedule), 'overall_elf_schedule_org.png')
+#PngIt(plot_duration(opt_schedule), 'toy_duration_opt.png')
+#PngIt(plot_toy_schedule(opt_schedule), 'overall_toy_schedule_opt.png')
+#PngIt(plot_elf_schedule(opt_schedule), 'overall_elf_schedule_opt.png')
+#PngIt(plot_elf_p_trend(opt_schedule), 'elf_p_trend_opt.png')
+
+#PngIt(plot_duration(org_schedule), 'toy_duration_org.png')
+#PngIt(plot_toy_schedule(org_schedule), 'overall_toy_schedule_org.png')
+#PngIt(plot_elf_schedule(org_schedule), 'overall_elf_schedule_org.png')
+#PngIt(plot_elf_p_trend(org_schedule), 'elf_p_trend_org.png')
+
+#print(system.time(org_toys     <- distribute_toys(n_elves, toys)))
+#org_toys<-org_toys[order(org_toys$ElfId,as.numeric(format(org_toys$Arrival,"%m")),org_toys$Duration)]
+#print(system.time(org_schedule <- build_schedule_c(org_toys, .parallel=T)))
+
+
 # PngIt(plot_elf_p_trend(org_schedule), 'elf_p_trend_org.png')
